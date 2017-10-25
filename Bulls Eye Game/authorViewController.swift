bfb045c768f205a4ab26c08cb6c6a79037feca48
Jonny Hork <JonnyHork@gmail.com>
2017-10-25 07:48:02 -0600

    All layouts are done. image loaded.

---
+++ Bulls Eye Game/authorViewController.swift
@@ -0,0 +1,39 @@
+//
+//  authorViewController.swift
+//  Bulls Eye Game
+//
+//  Created by Jonny Hork on 10/25/17.
+//  Copyright © 2017 G62-Jonny Hork. All rights reserved.
+//
+
+import UIKit
+
+class authorViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
