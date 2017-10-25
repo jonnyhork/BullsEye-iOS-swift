8669ebea734c34f398c1d439ad282d0983a8230b
Jonny Hork <JonnyHork@gmail.com>
2017-10-24 18:36:33 -0600

    Slider values are working for the alert message, connecting the target value to the targetLabel

--- Bulls Eye Game/ViewController.swift
+++ Bulls Eye Game/ViewController.swift
@@ -9,27 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 
     @IBAction func showAlert() {
-//        print("Hello World")
     
-    let alert = UIAlertController(title: "Hello World", message: "This is my first App!", preferredStyle: .alert)
+    let message = "The value of the slider is \(currentValue)" +
+        "\nThe Target Value is: \(targetValue) "
+    
+    let alert = UIAlertController(title: "Slider Value", message: message, preferredStyle: .alert)
     let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
     
         alert.addAction(action)
     
         present(alert, animated: true, completion: nil)
         
+        startNewRound()
     }
 
 
